64e6434e7ddd289eb5a3d8f50128d46cbbde638d
Elhamyali <elham.yusufali@gmail.com>
2023-07-30 15:59:11 -0700

    Analysis script and Rmd changed

--- Analysis Code.R
+++ Analysis Code.R
@@ -1,6 +1,30 @@
 # Install necessary packages
+install.packages("readxl")
 install.packages("dplyr")
+install.packages("tidyverse")
 
 # Load necessary libraries
+library(readxl)
 library(dplyr)
+library(tidyverse)
+
+# Both dataset1 and dataset2 are loaded into R with different formats for questions 8, 11, 12, and 13.
+
+# Read the datasets
+dataset1 <- read_excel("dataset 1.xlsx")
+View(dataset1)
+
+dataset2 <- read_excel("dataset 2.xlsx")
+View(dataset2)
+
+# Add a unique identifier column to distinguish the datasets
+dataset1$Dataset <- "Dataset 1"
+dataset2$Dataset <- "Dataset 2"
+
+# Rename headers from text to Q1, Q2, etc in both datasets
+# Rename the columns in dataset1
+colnames(dataset1)[colnames(dataset1) %in% c("question_8", "question_11", "question_12", "question_13")] <- c("QA8", "QA11", "QA12", "QA13")
+
+# Rename the columns in dataset2
+colnames(dataset2)[colnames(dataset2) %in% c("question_8", "question_11", "question_12", "question_13")] <- c("QA8", "QA11", "QA12", "QA13")
 
